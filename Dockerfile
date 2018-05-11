2c4f415e67fb95b8bc5da83d5b91cce6966ddb63
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2018-05-11 16:19:29 -0700

    add docker container

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM scratch
+COPY stbuild/cloudtail /bin/cloudtail
+CMD /bin/cloudtail
---
+++ travisbuild.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -x
+
+gox -os="linux windows" -arch="amd64" -output="./build/{{.Dir}}-{{.OS}}-{{.Arch}}"
+gox -os="darwin" -arch="amd64" -output="./build/{{.Dir}}-macOS-{{.Arch}}"
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o stbuild/cloudtail ..
+docker build . -t cloudtail
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker tag cloudtail tinyzimmer/cloudtail
+docker push tinyzimmer/cloudtail

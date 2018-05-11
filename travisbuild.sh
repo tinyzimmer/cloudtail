3e96e4f4a8ff058b3940898887d87f51b02363ef
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2018-05-11 16:24:21 -0700

    fix travis yml

--- travisbuild.sh
+++ travisbuild.sh
@@ -6,7 +6,7 @@ set -x
 
 gox -os="linux windows" -arch="amd64" -output="./build/{{.Dir}}-{{.OS}}-{{.Arch}}"
 gox -os="darwin" -arch="amd64" -output="./build/{{.Dir}}-macOS-{{.Arch}}"
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o stbuild/cloudtail ..
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o stbuild/cloudtail .
 docker build . -t cloudtail
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker tag cloudtail tinyzimmer/cloudtail
